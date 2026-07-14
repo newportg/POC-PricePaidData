055e6ac6dd1d59cf5d68f8bbbab98337f1ba30ec
Gary Newport <Gary.Newport@zoomalong.co.uk>
2026-07-14 15:17:18 +0100

    vault backup: 2026-07-14 15:17:18

--- Src/Server/Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ Src/Server/Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84f6fbec825813541c452e8ea9e999eb2141c8ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2b0ad2690cece8cba324d5ee86b1d573665d770")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
